49637d0a89028a5e84c9fd71bdd19b4cb8df6a3c
poojasahu021994 <poojasahu02071994@gmail.com>
2025-04-01 14:01:02 +0530

    update

--- project/app/urls.py
+++ project/app/urls.py
@@ -7,16 +7,17 @@ urlpatterns=[
     path ('Registration/', views.Registration,name='Registration'),
     path ('admindashboard/', views.admindashboard,name='admindashboard'),
     path ('userdashboard/<int:pk>', views.UserDashbashboard,name='userdashboard'),
-    path('profile/<int:pk>/',views.profile, name='profile'),
-    path('addtask/',views.Addtask, name='addtask'),
-    path('givetask/<int:pk>',views.givetask,name='givetask'),
-    path('remove/<int:pk>',views.remove,name='remove'),
-    path('logout/',views.Logout,name='logout'),
-    path('showtask/',views.Showtask, name='showtask'),
-    path('usertask/<int:pk>/',views.usertask, name='usertask'),
-    path('edituser/<int:pk>/',views.edituser, name='edituser'),
-    path('updatedata/<int:pk>/',views.updatedata,name='updatedata'),
-    path('update/<int:pk>/',views.update,name='update'),
+    path ('profile/<int:pk>/',views.profile, name='profile'),
+    path ('addtask/',views.Addtask, name='addtask'),
+    path ('givetask/<int:pk>',views.givetask,name='givetask'),
+    path ('remove/<int:pk>',views.remove,name='remove'),
+    path ('logout/',views.Logout,name='logout'),
+    path ('showtask/',views.Showtask, name='showtask'),
+    path ('usertask/<int:pk>/',views.usertask, name='usertask'),
+    path ('edituser/<int:pk>/',views.edituser, name='edituser'),
+    path ('updatedata/<int:pk>/',views.updatedata,name='updatedata'),
+    path 
+    ('update/<int:pk>/',views.update,name='update'),
 
 
 
